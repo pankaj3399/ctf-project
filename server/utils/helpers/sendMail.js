d0303d09e198ba823ee1bffa2463f853eac0d3a2
Pankaj Vatwani <vatwanipankaj74@gmail.com>
2024-03-24 00:58:34 +0530

    Fixes

--- server/utils/helpers/sendMail.js
+++ server/utils/helpers/sendMail.js
@@ -1,34 +1,29 @@
 // create a sendMail helper function that will send an email to the user with the otp.
 
 // Path: server/utils/helpers/sendMail.js
-import nodemailer from 'nodemailer';
-import config from './../server/config.js';
+import nodemailer from "nodemailer";
+
+import config from "./../server/config.js";
 
 const transporter = nodemailer.createTransport({
-
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
-    auth: {
-        user: config.EMAIL,
-        pass: config.EMAIL_PASSWORD
-    }
-   
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: config.SMTP_EMAIL,
+    pass: config.SMTP_PASSWORD,
+  },
 });
 
 const sendMail = async (to, otp) => {
+  const mailOptions = {
+    from: "vatwanipankaj74@gmail.com",
+    to: to,
+    subject: "Account Verification OTP",
+    text: `Your OTP is ${otp}`,
+  };
 
-
-    const mailOptions = {
-        from: config.EMAIL,
-        to: to,
-        subject: 'Account Verification OTP',
-        text: `Your OTP is ${otp}`
-    };
-
-    const res = await transporter.sendMail(mailOptions);
-}
-
-
+  const res = await transporter.sendMail(mailOptions);
+};
 
 export default sendMail;
